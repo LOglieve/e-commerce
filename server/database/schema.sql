8737f732360fb34d51e20e6aba66adecfc63d62b
Lily <lilymoglievechan@gmail.com>
2021-05-18 15:13:31 +0100

    database setup

---
+++ server/database/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE orders(
+  id serial unique,
+  order_date date,
+  total int,
+  product_ids integer[],
+  user_id int references users(id)
+ );
+
+ CREATE TABLE users(
+  id serial unique,
+  email varchar(64) unique,
+  password varchar(32)
+);
+
+CREATE TABLE products(
+  id serial unique,
+  description varchar(255),
+  quantity int,
+  price int 
+);
