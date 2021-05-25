f7edd012022e88c95ffe2b9bbe2a59231a9f004f
Lily <lilymoglievechan@gmail.com>
2021-05-25 15:30:03 +0100

    products fetched

--- client/src/components/shop.js
+++ client/src/components/shop.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Product from './product'
 
 export default function Shop(props) {
+    const [products, setProducts] = useState({});
+
+    const getProducts = async() =>{
+        try{
+            fetch('./products').then(response => {
+                response.json().then(response => {
+                    setProducts(response);
+                    console.log(products);
+                });
+
+            });
+        }catch(error){
+
+        }
+
+    }
+
+    useEffect(() =>{
+        getProducts();
+
+    }, []);
+
+
+
+
     return(
         <div>
             <h1>{props.category}</h1>
+            
 
             
 
